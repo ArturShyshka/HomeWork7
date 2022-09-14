33cde44af6b5b67dea696e0fea2c22a392790cf5
ArturShyshka <ashushka3@gmail.com>
2022-09-14 20:56:40 +0300

    Initial Commit

---
+++ Homework7_Task1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Homework7_Task1
+//
+//  Created by Artur Shyshka on 14.09.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
